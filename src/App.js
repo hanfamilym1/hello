1be1dd95a15c94f9e57986f8b293ac5f128384e8
hanfamilym1 <michael@hanfamily.com>
2018-09-14 15:07:25 -0600

    Trying to get done faster...

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Dashboard from './Components/Dashboard/Dashboard'
+import Auth from './Components/Auth/Auth'
+import Form from './Components/Form/Form'
+import Post from './Components/Post/Post'
+import Nav from './Components/Nav/Nav'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Nav/>
+        <Dashboard/>
+        <Form/>
+        <Auth/>
+        <Post/>
       </div>
     );
   }
